a397a564a9d2f44be5672f89116258f75a0992d7
Drew Henderson <drewhendersonemail@gmail.com>
2022-01-28 12:44:13 -0800

    added order form list

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,10 @@ function Pizza(toppings, size) {
 Pizza.prototype.getCost = function() {
   return ((10 + this.toppings.length) * this.size);
 }
+
+$(document).ready(function() {
+  $("#pizza-form").submit(function(event) {
+    event.preventDefault();
+    $("#pizza-form").hide();
+  });
+});
